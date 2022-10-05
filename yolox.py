c038f71913cb109cf4b608cc15bb2633c22ab909
Sarvagya Malaviya <malaviyasarvagya22@gmail.com>
2022-10-05 15:43:36 +0530

    added _backward

--- yolox.py
+++ yolox.py
@@ -198,8 +198,379 @@ class YoloX(ObjectDetectionBaseModel):
         )
         self.compiled_metrics.update_state(y_true, y_pred)
 
-    def compute_losses(self, y_true, y_pred, input_shape):
-        pass
+    def get_assignments(
+        self,
+        gt_bboxes_per_image,
+        gt_classes,
+        bboxes_preds_per_image,
+        obj_preds_per_image,
+        cls_preds_per_image,
+        x_shifts,
+        y_shifts,
+        expanded_strides,
+        num_classes,
+        num_gt,
+        total_num_anchors,
+    ):
+        fg_mask, is_in_boxes_and_center = self.get_in_boxes_info(
+            gt_bboxes_per_image,
+            x_shifts,
+            y_shifts,
+            expanded_strides,
+            num_gt,
+            total_num_anchors,
+        )
+
+        bboxes_preds_per_image = tf.boolean_mask(
+            bboxes_preds_per_image, fg_mask, axis=0
+        )
+        obj_preds_ = tf.boolean_mask(obj_preds_per_image, fg_mask, axis=0)
+        cls_preds_ = tf.boolean_mask(cls_preds_per_image, fg_mask, axis=0)
+        num_in_boxes_anchor = tf.shape(bboxes_preds_per_image)[0]
+
+        pair_wise_ious = bounding_box.compute_iou(
+            gt_bboxes_per_image, bboxes_preds_per_image, self.bounding_box_format
+        )
+        pair_wise_ious_loss = -tf.log(pair_wise_ious + 1e-8)
+
+        gt_cls_per_image = tf.tile(
+            tf.expand_dims(tf.one_hot(tf.cast(gt_classes, tf.int32), num_classes), 1),
+            (1, num_in_boxes_anchor, 1),
+        )
+        cls_preds_ = K.sigmoid(
+            tf.tile(tf.expand_dims(cls_preds_, 0), (num_gt, 1, 1))
+        ) * K.sigmoid(tf.tile(tf.expand_dims(obj_preds_, 0), (num_gt, 1, 1)))
+
+        pair_wise_cls_loss = tf.reduce_sum(
+            K.binary_crossentropy(gt_cls_per_image, tf.sqrt(cls_preds_)), -1
+        )
+
+        cost = (
+            pair_wise_cls_loss
+            + 3.0 * pair_wise_ious_loss
+            + 100000.0
+            * tf.cast((~is_in_boxes_and_center), K.dtype(bboxes_preds_per_image))
+        )
+
+        (
+            gt_matched_classes,
+            fg_mask,
+            pred_ious_this_matching,
+            matched_gt_inds,
+            num_fg,
+        ) = self.dynamic_k_matching(cost, pair_wise_ious, fg_mask, gt_classes, num_gt)
+        return (
+            gt_matched_classes,
+            fg_mask,
+            pred_ious_this_matching,
+            matched_gt_inds,
+            num_fg,
+        )
+
+    def get_in_boxes_info(
+        self,
+        gt_bboxes_per_image,
+        x_shifts,
+        y_shifts,
+        expanded_strides,
+        num_gt,
+        total_num_anchors,
+        center_radius=2.5,
+    ):
+        expanded_strides_per_image = expanded_strides[0]
+        x_centers_per_image = tf.tile(
+            tf.expand_dims(((x_shifts[0] + 0.5) * expanded_strides_per_image), 0),
+            [num_gt, 1],
+        )
+        y_centers_per_image = tf.tile(
+            tf.expand_dims(((y_shifts[0] + 0.5) * expanded_strides_per_image), 0),
+            [num_gt, 1],
+        )
+
+        gt_bboxes_per_image_l = tf.tile(
+            tf.expand_dims(
+                (gt_bboxes_per_image[:, 0] - 0.5 * gt_bboxes_per_image[:, 2]), 1
+            ),
+            [1, total_num_anchors],
+        )
+        gt_bboxes_per_image_r = tf.tile(
+            tf.expand_dims(
+                (gt_bboxes_per_image[:, 0] + 0.5 * gt_bboxes_per_image[:, 2]), 1
+            ),
+            [1, total_num_anchors],
+        )
+        gt_bboxes_per_image_t = tf.tile(
+            tf.expand_dims(
+                (gt_bboxes_per_image[:, 1] - 0.5 * gt_bboxes_per_image[:, 3]), 1
+            ),
+            [1, total_num_anchors],
+        )
+        gt_bboxes_per_image_b = tf.tile(
+            tf.expand_dims(
+                (gt_bboxes_per_image[:, 1] + 0.5 * gt_bboxes_per_image[:, 3]), 1
+            ),
+            [1, total_num_anchors],
+        )
+
+        b_l = x_centers_per_image - gt_bboxes_per_image_l
+        b_r = gt_bboxes_per_image_r - x_centers_per_image
+        b_t = y_centers_per_image - gt_bboxes_per_image_t
+        b_b = gt_bboxes_per_image_b - y_centers_per_image
+        bbox_deltas = tf.stack([b_l, b_t, b_r, b_b], 2)
+
+        is_in_boxes = tf.reduce_min(bbox_deltas, axis=-1) > 0.0
+        is_in_boxes_all = (
+            tf.reduce_sum(tf.cast(is_in_boxes, K.dtype(gt_bboxes_per_image)), axis=0)
+            > 0.0
+        )
+
+        gt_bboxes_per_image_l = tf.tile(
+            tf.expand_dims(gt_bboxes_per_image[:, 0], 1), [1, total_num_anchors]
+        ) - center_radius * tf.expand_dims(expanded_strides_per_image, 0)
+        gt_bboxes_per_image_r = tf.tile(
+            tf.expand_dims(gt_bboxes_per_image[:, 0], 1), [1, total_num_anchors]
+        ) + center_radius * tf.expand_dims(expanded_strides_per_image, 0)
+        gt_bboxes_per_image_t = tf.tile(
+            tf.expand_dims(gt_bboxes_per_image[:, 1], 1), [1, total_num_anchors]
+        ) - center_radius * tf.expand_dims(expanded_strides_per_image, 0)
+        gt_bboxes_per_image_b = tf.tile(
+            tf.expand_dims(gt_bboxes_per_image[:, 1], 1), [1, total_num_anchors]
+        ) + center_radius * tf.expand_dims(expanded_strides_per_image, 0)
+
+        c_l = x_centers_per_image - gt_bboxes_per_image_l
+        c_r = gt_bboxes_per_image_r - x_centers_per_image
+        c_t = y_centers_per_image - gt_bboxes_per_image_t
+        c_b = gt_bboxes_per_image_b - y_centers_per_image
+        center_deltas = tf.stack([c_l, c_t, c_r, c_b], 2)
+
+        is_in_centers = tf.reduce_min(center_deltas, axis=-1) > 0.0
+        is_in_centers_all = (
+            tf.reduce_sum(tf.cast(is_in_centers, K.dtype(gt_bboxes_per_image)), axis=0)
+            > 0.0
+        )
+
+        fg_mask = tf.cast(is_in_boxes_all | is_in_centers_all, tf.bool)
+
+        is_in_boxes_and_center = tf.boolean_mask(
+            is_in_boxes, fg_mask, axis=1
+        ) & tf.boolean_mask(is_in_centers, fg_mask, axis=1)
+
+        return fg_mask, is_in_boxes_and_center
+
+    def dynamic_k_matching(self, cost, pair_wise_ious, fg_mask, gt_classes, num_gt):
+        matching_matrix = tf.zeros_like(cost)
+        n_candidate_k = tf.minimum(10, tf.shape(pair_wise_ious)[1])
+        topk_ious, _ = tf.nn.top_k(pair_wise_ious, n_candidate_k)
+        dynamic_ks = tf.maximum(tf.reduce_sum(topk_ious, 1), 1)
+
+        def loop_body_1(b, matching_matrix):
+            _, pos_idx = tf.nn.top_k(-cost[b], k=tf.cast(dynamic_ks[b], tf.int32))
+            matching_matrix = tf.concat(
+                [
+                    matching_matrix[:b],
+                    tf.expand_dims(
+                        tf.reduce_max(tf.one_hot(pos_idx, tf.shape(cost)[1]), 0), 0
+                    ),
+                    matching_matrix[b + 1 :],
+                ],
+                axis=0,
+            )
+            return b + 1, matching_matrix
+
+        _, matching_matrix = tf.while_loop(
+            lambda b, *args: b < tf.cast(num_gt, tf.int32),
+            loop_body_1,
+            [0, matching_matrix],
+        )
+
+        anchor_matching_gt = tf.reduce_sum(matching_matrix, 0)
+        biger_one_indice = tf.reshape(tf.where(anchor_matching_gt > 1), [-1])
+
+        def loop_body_2(b, matching_matrix):
+            indice_anchor = tf.cast(biger_one_indice[b], tf.int32)
+            indice_gt = tf.math.argmin(cost[:, indice_anchor])
+            matching_matrix = tf.concat(
+                [
+                    matching_matrix[:, :indice_anchor],
+                    tf.expand_dims(tf.one_hot(indice_gt, tf.cast(num_gt, tf.int32)), 1),
+                    matching_matrix[:, indice_anchor + 1 :],
+                ],
+                axis=-1,
+            )
+            return b + 1, matching_matrix
+
+        _, matching_matrix = tf.while_loop(
+            lambda b, *args: b < tf.cast(tf.shape(biger_one_indice)[0], tf.int32),
+            loop_body_2,
+            [0, matching_matrix],
+        )
+
+        fg_mask_inboxes = tf.reduce_sum(matching_matrix, 0) > 0.0
+        num_fg = tf.reduce_sum(tf.cast(fg_mask_inboxes, K.dtype(cost)))
+
+        fg_mask_indices = tf.reshape(tf.where(fg_mask), [-1])
+        fg_mask_inboxes_indices = tf.reshape(tf.where(fg_mask_inboxes), [-1, 1])
+        fg_mask_select_indices = tf.gather_nd(fg_mask_indices, fg_mask_inboxes_indices)
+        fg_mask = tf.cast(
+            tf.reduce_max(tf.one_hot(fg_mask_select_indices, tf.shape(fg_mask)[0]), 0),
+            K.dtype(fg_mask),
+        )
+
+        matched_gt_inds = tf.math.argmax(
+            tf.boolean_mask(matching_matrix, fg_mask_inboxes, axis=1), 0
+        )
+        gt_matched_classes = tf.gather_nd(
+            gt_classes, tf.reshape(matched_gt_inds, [-1, 1])
+        )
+
+        pred_ious_this_matching = tf.boolean_mask(
+            tf.reduce_sum(matching_matrix * pair_wise_ious, 0), fg_mask_inboxes
+        )
+        return (
+            gt_matched_classes,
+            fg_mask,
+            pred_ious_this_matching,
+            matched_gt_inds,
+            num_fg,
+        )
+
+    def compute_losses(self, y_true, y_pred, input_shape=(640, 640)):
+        num_levels = len(y_pred)
+
+        if y_true.shape[-1] != 5:
+            raise ValueError(
+                "y_true should have shape (None, None, 5).  Got "
+                f"y_true.shape={tuple(y_true.shape)}"
+            )
+
+        for i in range(num_levels):
+            if y_pred[i].shape[-1] != self.classes + 5:
+                raise ValueError(
+                    "y_pred should be a list with tensors of shape (None, None, None, classes + 5). "
+                    f"Got y_pred[{i}].shape={tuple(y_pred[i].shape)}.  Does your model's `classes` "
+                    "parameter match your losses `classes` parameter?"
+                )
+
+        for i in range(num_levels):
+            output = y_pred[i]
+
+            grid_shape = tf.shape(output)[1:3]
+            stride = input_shape[0] / tf.cast(grid_shape[0], tf.float32)
+
+            grid_x, grid_y = tf.meshgrid(
+                tf.range(grid_shape[1]), tf.range(grid_shape[0])
+            )
+            grid = tf.cast(
+                tf.reshape(tf.stack((grid_x, grid_y), 2), (1, -1, 2)), tf.float32
+            )
+
+            output = tf.reshape(
+                output, [tf.shape(y_pred[i])[0], grid_shape[0] * grid_shape[1], -1]
+            )
+            output_xy = (output[..., :2] + grid) * stride
+            output_wh = tf.exp(output[..., 2:4]) * stride
+            output = tf.concat([output_xy, output_wh, output[..., 4:]], -1)
+
+            x_shifts.append(grid[..., 0])
+            y_shifts.append(grid[..., 1])
+            expanded_strides.append(tf.ones_like(grid[..., 0]) * stride)
+            outputs.append(output)
+
+        x_shifts = tf.concat(x_shifts, 1)
+        y_shifts = tf.concat(y_shifts, 1)
+        expanded_strides = tf.concat(expanded_strides, 1)
+        outputs = tf.concat(outputs, 1)
+
+        bbox_preds = outputs[:, :, :4]
+        obj_preds = outputs[:, :, 4:5]
+        cls_preds = outputs[:, :, 5:]
+
+        num_labels = tf.reduce_sum(tf.cast(tf.reduce_sum(y_true, -1) > 0, tf.int32), -1)
+        total_num_anchors = outputs.shape[1]
+
+        num_fg = 0.0
+        loss_obj = 0.0
+        loss_cls = 0.0
+        loss_iou = 0.0
+        batch_size = tf.shape(outputs[0])
+
+        for b in range(batch_size):
+            num_gt = tf.cast(num_labels[b], tf.int32)
+
+            gt_bboxes_per_image = y_true[b][:num_gt, :4]
+            gt_classes = y_true[b][:num_gt, 4]
+            bboxes_preds_per_image = bbox_preds[b]
+            obj_preds_per_image = obj_preds[b]
+            cls_preds_per_image = cls_preds[b]
+
+            if num_gt == 0:
+                num_fg_img = tf.cast(tf.constant(0), K.dtype(outputs))
+                cls_target = tf.cast(tf.zeros((0, self.classes)), K.dtype(outputs))
+                reg_target = tf.cast(tf.zeros((0, 4)), K.dtype(outputs))
+                obj_target = tf.cast(tf.zeros((total_num_anchors, 1)), K.dtype(outputs))
+                fg_mask = tf.cast(tf.zeros(total_num_anchors), tf.bool)
+            else:
+                (
+                    gt_matched_classes,
+                    fg_mask,
+                    pred_ious_this_matching,
+                    matched_gt_inds,
+                    num_fg_img,
+                ) = self.get_assignments(
+                    gt_bboxes_per_image,
+                    gt_classes,
+                    bboxes_preds_per_image,
+                    obj_preds_per_image,
+                    cls_preds_per_image,
+                    x_shifts,
+                    y_shifts,
+                    expanded_strides,
+                    self.classes,
+                    num_gt,
+                    total_num_anchors,
+                )
+                reg_target = tf.cast(
+                    tf.gather_nd(
+                        gt_bboxes_per_image, tf.reshape(matched_gt_inds, [-1, 1])
+                    ),
+                    K.dtype(outputs),
+                )
+                cls_target = tf.cast(
+                    tf.one_hot(tf.cast(gt_matched_classes, tf.int32), self.classes)
+                    * tf.expand_dims(pred_ious_this_matching, -1),
+                    K.dtype(outputs),
+                )
+                obj_target = tf.cast(tf.expand_dims(fg_mask, -1), K.dtype(outputs))
+
+            num_fg += num_fg_img
+            loss_iou += tf.math.reduce_sum(
+                1
+                - self.box_loss(
+                    reg_target, tf.boolean_mask(bboxes_preds_per_image, fg_mask)
+                )
+            )
+            loss_obj += tf.math.reduce_sum(
+                self.objectness_loss(obj_target, obj_preds_per_image, from_logits=True)
+            )
+            loss_cls += tf.math.reduce_sum(
+                self.classification_loss(
+                    cls_target,
+                    tf.boolean_mask(cls_preds_per_image, fg_mask),
+                    from_logits=True,
+                )
+            )
+
+        self.classification_loss_metric.update_state(loss_cls)
+        self.box_loss_metric.update_state(loss_iou)
+        self.regularization_loss_metric.update_state(regularization_loss)
+
+        num_fg = tf.cast(tf.maximum(num_fg, 1), K.dtype(outputs))
+        regularization_loss = 0.0
+        for loss in self.losses:
+            regularization_loss += tf.nn.scale_regularization_loss(loss)
+        loss = loss_iou + loss_obj + loss_cls + regularization_loss
+
+        return loss / num_fg
 
     def _backward(self, y_true, y_pred, input_shape):
         loss = self.compute_losses(y_true, y_pred, input_shape=input_shape)
